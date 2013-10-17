5f9af075cd43c3fc000ae2527eaeb2510d6607f8
smilingjb12 <smilingjb12@gmail.com>
2013-10-17 16:59:45 +0300

    Implemented signup with email activation

--- FriendsApp/Controllers/AccountController.cs
+++ FriendsApp/Controllers/AccountController.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using FriendsApp.ViewModels;
+using FriendsApp.Models;
 
 namespace FriendsApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly AppEntities db;
+        private readonly EmailSender mailSender;
+
+        public AccountController(AppEntities db, EmailSender mailSender)//, EmailSender mailSender)
+        {
+            this.db = db;
+            this.mailSender = mailSender;
+        }
+
+        public RedirectToRouteResult Activate(string code)
+        {
+            User user = db.Users.FirstOrDefault(u => u.ActivationCode == code);
+            if (user == null)
+            {
+                TempData["error"] = "No user with given activation code was found";
+                return RedirectToAction("Index", "Home");
+            }
+            user.IsActivated = true;
+            TempData["success"] = "Your account has been activated. You can sign in now.";
+            return RedirectToAction("Index", "Home");
+        }
+
         public ViewResult SignUp()
         {
             return View();
@@ -21,6 +46,33 @@ namespace FriendsApp.Controllers
             {
                 return View(model);
             }
+            User existingUser = db.Users.FirstOrDefault(u => u.Email == model.Email);
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("", "Email is already taken. Choose another.");
+                return View(model);
+            }
+            string activationCode = Guid.NewGuid().ToString();
+            try
+            {
+                mailSender.SendActivationCode(model.Email, activationCode);
+            }
+            catch (FormatException)
+            {
+                ModelState.AddModelError("Email", "Email format is incorrect");
+                return View(model);
+            }
+            string hashedPassword = FormsAuthentication.HashPasswordForStoringInConfigFile(model.Password, "SHA1");
+            User newUser = new User();
+            newUser.Email = model.Email;
+            newUser.Password = hashedPassword;
+            newUser.ActivationCode = activationCode;
+            newUser.IsActivated = false;
+            newUser.Role = "User";
+            db.Users.Add(newUser);
+            db.SaveChanges();
+            TempData["success"] = string.Format("An email with activation link has been sent to {0}. Check it out.",
+                model.Email);
             return RedirectToAction("Index", "Home");
         }
     }
---
+++ FriendsApp/Models/FriendsAppEntities.edmx.sql
@@ -0,0 +1,60 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, and Azure
+-- --------------------------------------------------
+-- Date Created: 10/17/2013 15:53:27
+-- Generated from EDMX file: C:\Users\Fluffy\Documents\Visual Studio 2012\Projects\FriendsApp\FriendsApp\Models\FriendsAppEntities.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [FriendsTestDb];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Users]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Users];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Email] nvarchar(max)  NOT NULL,
+    [Password] nvarchar(max)  NOT NULL,
+    [ActivationCode] nvarchar(max)  NOT NULL,
+    [IsActivated] bit  NOT NULL,
+    [Role] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
