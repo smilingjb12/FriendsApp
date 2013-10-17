023bd296eae49382f0b78981e34b61ec65da0864
smilingjb12 <smilingjb12@gmail.com>
2013-10-17 13:13:52 +0300

    Main layout setup

---
+++ FriendsApp/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FriendsApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(int x)
+        {
+            return null;
+        }
+    }
+}
